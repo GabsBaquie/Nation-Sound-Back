63062b3d618cc2f0580644c31098716e6a95f3f5
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-19 18:04:00 +0200

    chore: Update vercel-deploy plugin configuration

--- strapi-CMS/config/plugins.ts
+++ strapi-CMS/config/plugins.ts
@@ -16,7 +16,11 @@ module.exports = ({ env }) => ({
       "vercel-deploy": {
         enabled: true,
         config: {
-          deployHook: "https://nation-sound-back.vercel.app/",
+          deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+          apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+          appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+          teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+          roles: ["strapi-super-admin"],
         },
       },
     },
