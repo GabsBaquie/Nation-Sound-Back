da0aa1e7a2dec93b1fed5f912f71f466008e1fc9
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-19 19:40:28 +0200

    chore: Update vercel.json and remove unused dependencies

--- strapi-CMS/config/plugins.ts
+++ strapi-CMS/config/plugins.ts
@@ -13,16 +13,6 @@ module.exports = ({ env }) => ({
         removeAttributesKey: true,
         removeDataKey: true,
       },
-      "vercel-deploy": {
-        enabled: true,
-        config: {
-          deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-          apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-          appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-          teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
-          roles: ["strapi-super-admin"],
-        },
-      },
     },
   },
 });
