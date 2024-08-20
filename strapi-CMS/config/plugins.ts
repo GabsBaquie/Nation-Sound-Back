5b436663d9d24d8bb8114d335d06a97925e87931
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-20 16:06:56 +0200

    chore: Update generated components.d.ts file with new component definitions

--- strapi-CMS/config/plugins.ts
+++ strapi-CMS/config/plugins.ts
@@ -1,10 +1,7 @@
-export default () => ({
+export default ({ env }) => ({
   "wysiwyg-react-md-editor": {
     enabled: true,
   },
-});
-
-module.exports = ({ env }) => ({
   transformer: {
     enabled: true,
     config: {
