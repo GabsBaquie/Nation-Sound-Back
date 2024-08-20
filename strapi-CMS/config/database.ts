756f7a4f448da32dab4c6d19de63f7f82f5ebf6f
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-20 14:43:02 +0200

    chore: Update database configuration for MySQL connection

--- strapi-CMS/config/database.ts
+++ strapi-CMS/config/database.ts
@@ -2,28 +2,17 @@ export default ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
 
   const connections = {
-    postgres: {
+    mysql: {
+      client: env("DATABASE_CLIENT", "mysql"),
       connection: {
-        host: env(
-          "DATABASE_HOST",
-          "ep-aged-hall-a2c7hnmf.eu-central-1.aws.neon.tech"
-        ),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "NationSound"),
-        user: env("DATABASE_USERNAME", "NationSound_owner"),
-        password: env("DATABASE_PASSWORD", "v3ADRVWmUHl8"),
-        ssl: {
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "NationSounds"),
+        user: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "root"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      debug: false,
     },
   };
 
