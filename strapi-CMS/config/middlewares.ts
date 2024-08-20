193ee905db8d13bf969fa2160330a8aa4aa0a05b
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-20 15:17:23 +0200

    chore: Update npm dependencies and add sqlite3 package

--- strapi-CMS/config/middlewares.ts
+++ strapi-CMS/config/middlewares.ts
@@ -2,12 +2,6 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["https://nation-sound-front.vercel.app"], //  l'URL du frontend
-    },
-  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
