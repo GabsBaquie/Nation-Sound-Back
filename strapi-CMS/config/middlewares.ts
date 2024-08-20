934c98606604b424a6b4294a0cf27ab5d40d49f5
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-20 15:25:49 +0200

    chore: Update strapi::cors middleware configuration for enabling Cross-Origin Resource Sharing (CORS)

--- strapi-CMS/config/middlewares.ts
+++ strapi-CMS/config/middlewares.ts
@@ -2,7 +2,12 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://nation-sound-back.onrender.com"], //  l'URL du frontend
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
