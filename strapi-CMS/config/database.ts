3f81a2535a4d1796cff770f3ad309199cafea326
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2024-08-21 14:43:57 +0200

    chore: Update database configuration to use mysql as the database client and disable SSL

--- strapi-CMS/config/database.ts
+++ strapi-CMS/config/database.ts
@@ -1,16 +1,17 @@
 export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
+  const client = env("DATABASE_CLIENT", "mysql");
 
   const connections = {
     mysql: {
       client: "mysql2",
       connection: {
         host: env("DATABASE_HOST", "autorack.proxy.rlwy.net"),
-        port: env.int("DATABASE_PORT", 3306),
+        port: env.int("DATABASE_PORT", 59624),
         database: env("DATABASE_NAME", "railway"),
         user: env("DATABASE_USERNAME", "root"),
         password: env("DATABASE_PASSWORD", "QMVgOBeAUjosMGEIBqHcSpJAkPMdUUkv"),
-        ssl: env.bool("DATABASE_SSL", true),
+        ssl: env.bool("DATABASE_SSL", false),
+        bigNumberStrings: true,
       },
       debug: false,
     },
